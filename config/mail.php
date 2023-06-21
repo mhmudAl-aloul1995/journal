c37c6e2029dbdd9dc51dccbe61b1befba39ba635
mhmudAl-aloul1995 <56793506+mhmudAl-aloul1995@users.noreply.github.com>
2023-06-21 13:12:42 +0300

    tyu

--- config/mail.php
+++ config/mail.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'driver' => env('MAIL_DRIVER', 'smtp'),
+    'driver' => env('MAIL_DRIVER', 'sendmail'),
     'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
     'port' => env('MAIL_PORT', 587),
     'from' => ['address' => 'yourEmail@gmail.com', 'name' => 'مجلة جامعة غزة للأبحاث والدراسات'],

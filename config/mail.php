e4c4c10da160db704ec36380f48f8281339b425c
mhmudAl-aloul1995 <56793506+mhmudAl-aloul1995@users.noreply.github.com>
2023-06-22 13:32:16 +0300

    ds

--- config/mail.php
+++ config/mail.php
@@ -38,7 +38,7 @@ return [
             'transport' => 'smtp',
             'host' => env('MAIL_HOST', 'smtp.gmail.com'),
             'port' => env('MAIL_PORT', 587),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'encryption' => env('MAIL_ENCRYPTION', 'ssl'),
             'username' => "mhmudaloul@gmail.com",
             'password' => "Qqqq1111@",
             'timeout' => null,
